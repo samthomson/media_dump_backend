8ca39c266c6c4f25f41b04b5c576116baed31263
samthomson <samt@samt.st>
2014-10-08 19:31:36 +0200

    adjusted search handler to understand face/body queries

--- media_dump/views.py
+++ media_dump/views.py
@@ -187,6 +187,8 @@ def search(request):
 					l_queries.append({"longitude": {"$lt": float(fl_values[3]) }})
 					
 			else:
+				if s_type == "face":
+					s_value = s_value.lower() in ("yes", "true", "t", "1")
 				# add default straight equals query
 				l_queries.append({ "$and" : [ { "tags.type": s_type }, { "tags.value": s_value } ] })
 
