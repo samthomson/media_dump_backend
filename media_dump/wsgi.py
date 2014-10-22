2d371ee5a2b5a77d714a53129489887029bf899d
samthomson <samt@samt.st>
2014-10-22 22:14:03 +0200

    changed wsgi to run as apache app

--- media_dump/wsgi.py
+++ media_dump/wsgi.py
@@ -1,9 +1,26 @@
+activate_this = '/home/sam/.virtualenvs/media_dump_env/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+
+
 import os
+
+
+
+
+
 import sys	
+
+path = '/var/www/media_dump'
+if path not in sys.path:
+    sys.path.append(path)
+
 
 sys.path.insert(0, '/var/www/media_dump_app')
 
 sys.path.append('~/var/www/mdaclient/')
+
+
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'media_dump.settings'
 #import django.core.handlers.wsgi
 #application = django.core.handlers.wsgi.WSGIHandler()
